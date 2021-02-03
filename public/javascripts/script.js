11f9567b37f270d7824974e7e155a2b0c600f632
oezge sebisteri <sebisteri@outlook.com>
2021-02-03 01:53:27 +0100

    Keine Ahnung was das alles bedeutet.

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -10,7 +10,20 @@ for (let i = 0; i < mashPotatoes.length; i++) {
 addFood(steak[0], '#steak', () => {
   // ... your code here
   addFood(steak[1], '#steak', () => {
-
+    addFood(steak[2], '#steak', () => {
+      addFood(steak[3], '#steak', () => {
+        addFood(steak[4], '#steak', () => {
+          addFood(steak[5], '#steak', () => {
+            addFood(steak[6], '#steak', () => {
+              addFood(steak[7], '#steak', () => {
+                console.log('list is finished');
+                document.querySelector('#table').innerHTML += '<img src="public/images/steak.jpg" alt=""/>'
+              })
+            })
+          })
+        })
+      })
+    })
   })
 });
 
@@ -19,13 +32,30 @@ addFood(steak[0], '#steak', () => {
 // Iteration 2 using `.then()`
 addFood(mashPotatoes[0], '#mashPotatoes').then(() => {
   // ... your code here
-  addFood(mashPotatoes[1], '#mashPotatoes')
+  addFood(mashPotatoes[1], '#mashPotatoes').then(() => {
+    addFood(mashPotatoes[2], '#mashPotatoes').then(() => {
+      addFood(mashPotatoes[3], '#mashPotatoes').then(() => {
+        addFood(mashPotatoes[4], '#mashPotatoes').then(() => {
+          console.log('list is finished');
+          document.querySelector('#table').innerHTML += '<img src="public/images/mashPotatoes.jpg" alt=""/>'
+        })
+      })
+    })
+  })
 });
 
 // Iteration 3 using async and await
 
-  async function makeFood(step) {
-    // ... your code here
-    
+async function makeFood(steps) {
+  for (let step of steps) {
+    await addFood(step, '#brusselSprouts');
   }
+  document.querySelector('#table').innerHTML += `<img src="public/images/brusselSprouts.jpg" alt="sprouts"/>`
+}
+
+makeFood(brusselSprouts, '#brusselSprouts');
+
+
+
+  
   makeFood(eachStep);
